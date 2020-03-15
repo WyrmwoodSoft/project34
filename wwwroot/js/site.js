5a41fde665beeaa8b2a6c411ca4d1510103b6952
matt <metropolitan.matt@gmail.com>
2020-03-15 11:17:47 -0400

    initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+particlesJS.load('particles-js', './particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
